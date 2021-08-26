840c8d2eaa711abda213726945799bf691fdb02c
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-08-26 15:09:44 +0700

    Update Join.js

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -10,12 +10,15 @@ export default function SignIn() {
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
-        <h1 className="heading">Join</h1>
+        <h1 className="heading">WEBCHAT - FREE CHAT ROOM</h1>
+        <h1 className="heading"> </h1>
+        <h1 className="heading"> </h1>
+        <h1 className="heading">Join or Create Room</h1>
         <div>
-          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+          <input placeholder="Nickname" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
         </div>
         <div>
-          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
+          <input placeholder="Room Name" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
         </div>
         <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
           <button className={'button mt-20'} type="submit">Sign In</button>
