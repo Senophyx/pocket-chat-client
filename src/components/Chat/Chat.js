421627e8497196370ce49cb50a01d40dac41687f
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-09-06 15:00:29 +0700

    Update Server

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -9,7 +9,7 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-const ENDPOINT = 'https://server-webchat.herokuapp.com/';
+const ENDPOINT = 'https://server-pocket-chat.herokuapp.com/';
 
 let socket;
 
