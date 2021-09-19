1ebb5e112eb160975ffcfc2995b4b148ddd29906
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2021-09-19 13:39:33 +0700

    Change server

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -9,7 +9,7 @@ import Input from '../Input/Input';
 
 import './Chat.css';
 
-const ENDPOINT = 'https://pocket-chat-server.vercel.app/';
+const ENDPOINT = 'https://server-pocket-chat.herokuapp.com/';
 
 let socket;
 
